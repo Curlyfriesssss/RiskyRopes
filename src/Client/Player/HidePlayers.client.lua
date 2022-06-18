744103b83514cd72087145a3415a79119f8b9272
TheBoogle <theboogledoo@gmail.com>
2022-06-17 17:02:40 -0700

    optimization

--- src/Client/Player/HidePlayers.client.lua
+++ src/Client/Player/HidePlayers.client.lua
@@ -7,23 +7,30 @@ local map = require(game.ReplicatedStorage.Modules.UI.Functions).Map
 
 function HideCharacter(Character: Model, Distance: number)
 	local MyRadius = shared.Settings.bubble_radius.Value
-	local NewTransparency = 1 - map(Distance, MyRadius/2, MyRadius, 0, 1)
+	local NewTransparency = 1 - map(Distance, MyRadius, MyRadius*2, 0, 1)
 
-	for _, BasePart: BasePart in Character:GetDescendants() do
-		if BasePart:IsA("BasePart") and BasePart.Name ~= 'HumanoidRootPart' then
-			BasePart.Transparency = NewTransparency
+	coroutine.wrap(function()
+		for _, BasePart: BasePart in ipairs(Character:GetDescendants()) do
+			if (BasePart:IsA("BasePart") or BasePart:IsA("Decal")) and BasePart.Name ~= 'HumanoidRootPart' then
+				BasePart.Transparency = NewTransparency
+			end
 		end
-	end
+	end)()
 end
 
-RS.RenderStepped:Connect(function(deltaTime)
+Camera:GetPropertyChangedSignal("CFrame"):Connect(function()
 	
+	local Players = Players:GetPlayers()
+
+	if #Players == 0 then return end
 	local MyPosition = Camera.CFrame.Position
 
-	for _, Player in Players:GetPlayers() do
+	for _, Player in ipairs(Players) do
 		if Player.Character and Player.Character:FindFirstChild("HumanoidRootPart") then
 			local PlayerPosition = Player.Character:FindFirstChild("HumanoidRootPart").Position
 			local Distance = (MyPosition - PlayerPosition).Magnitude
+
+			if Distance >= shared.Settings.bubble_radius.Default[1].Max then continue end
 
 			HideCharacter(Player.Character, Distance)
 		end
