7c8368939f2f4400bb47fd656617baa750c69143
TheBoogle <theboogledoo@gmail.com>
2022-07-11 19:29:17 -0700

    :CreateBin() and :GetURL()

--- src/ServerModules/Networking/JSONBin.lua
+++ src/ServerModules/Networking/JSONBin.lua
@@ -3,9 +3,29 @@
 	It's primary function for Risky Ropes is to save runs in a readable json
 	format and upload it as a URL to Discord
 
-]]--
+]]
+
+local HTTPS = game:GetService("HttpService")
+
+local Info = require(script.Parent.Parent.Data.JSONBinInfo)
 
 local JSONBin = {}
+JSONBin.__index = JSONBin
 
+local self = {}
 
-return JSONBin
+function JSONBin:GetURL(Response: any)
+	return Info.Public_URL .. Response.metadata.id
+end
+
+function JSONBin:CreateBin(JSON: string)
+	local Request = HTTPS:PostAsync(Info.URL, JSON, Enum.HttpContentType.ApplicationJson, false, Info.Headers)
+
+	Request = HTTPS:JSONDecode(Request)
+
+	return self:GetURL(Request)
+end
+
+setmetatable(self, JSONBin)
+
+return self
