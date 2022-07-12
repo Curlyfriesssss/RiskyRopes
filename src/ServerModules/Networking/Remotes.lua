158b5fc4db63766028d5b3c0cc5aadd3d6b42be7
TheBoogle <theboogledoo@gmail.com>
2022-07-11 23:59:04 -0700

    SetScore remote

--- src/ServerModules/Networking/Remotes.lua
+++ src/ServerModules/Networking/Remotes.lua
@@ -1,5 +1,10 @@
 local HTTPS = game:GetService("HttpService")
 
+local DiscordInfo = require(script.Parent.Parent.Data.DiscordWebhooks)
+
+local SharedModules = game.ReplicatedStorage.Modules
+local ExtraFunctions = require(SharedModules.UI.Functions)
+
 return function(ImportantData)
 	local LoadedModules = ImportantData.Mods
 	local UserAccounts = ImportantData.UserAccounts
@@ -7,6 +12,12 @@ return function(ImportantData)
 	local Remotes = game.ReplicatedStorage.Remotes
 
 	local StatCache = {}
+
+	local Leaderboards = LoadedModules.Leaderboards
+	local JSONBin = LoadedModules.JSONBin
+	local Discord = LoadedModules.Discord
+
+	local LeaderboardSize = 100
 
 	local RemotesToFunctions = {
 		[Remotes.GetSelfData] = function(Player: Player)
@@ -39,14 +50,31 @@ return function(ImportantData)
 			return Stats
 		end,
 		[Remotes.GetLeaderboard] = function(Player: Player, MapName: string, Page: number)
-			local Leaderboards = LoadedModules.Leaderboards
-
-			local LeaderboardSize = 100
-
 			local FetchedData = Leaderboards:Get(MapName:lower():gsub("%s", "_"), LeaderboardSize, Page or 0)
 
 			return HTTPS:JSONDecode(FetchedData.Body)
 		end,
+		[Remotes.SetScore] = function(Player: Player, MapName: string, Score: number)
+			-- TODO: Implement some security here
+			local Result = Leaderboards:Set(MapName, Player.UserId, Score)
+
+			if Result.StatusCode == 200 then
+				local Embed = Discord.embed()
+				
+				Embed.Title = "__New Time Achieved__"
+
+				Embed:AddField('Username',("`@%s`"):format(Player.Name),true)
+				Embed:AddField('User ID',("`%s`"):format(Player.UserId),true)
+
+				Embed:AddField('Map',("`%s`"):format(MapName),false)
+				Embed:AddField('New Time',("`%s`"):format(ExtraFunctions.ScoreToReadable(Score)),false)
+
+				Embed.Color = Color3.new(0, 1, 0.517647)
+				
+				local Webhook = Discord.webhook(DiscordInfo.Webhooks[MapName:lower():gsub("%s", "_")])
+				Webhook:send("", Embed)
+			end
+		end
 	}
 
 	for Remote, FunctionToRun in RemotesToFunctions do
