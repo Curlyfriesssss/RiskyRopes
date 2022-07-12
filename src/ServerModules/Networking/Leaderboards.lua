b0d27bcd42f78345351d9977af203044d8412b44
TheBoogle <theboogledoo@gmail.com>
2022-07-11 20:26:56 -0700

    Update Leaderboards.lua

--- src/ServerModules/Networking/Leaderboards.lua
+++ src/ServerModules/Networking/Leaderboards.lua
@@ -4,6 +4,7 @@ local Leaderboards = {}
 
 function Leaderboards:Get(MapName: string, Limit: number, Page: number)
 	Page -= 1
+	
 	return WebServer:Get(("api/leaderboard/%s/%s?page=%s"):format(MapName, Limit, Page))
 end
 
